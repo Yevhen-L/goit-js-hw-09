7c01f40f050e92ff81080e9875c95bb321a090d5
Yevhen-L <127352667+Yevhen-L@users.noreply.github.com>
2023-08-11 12:06:45 +0000

    Deploying to gh-pages from @ Yevhen-L/goit-js-hw-09@d5b412d2e73721ba5bff3c42e149af04e36598ad 🚀

---
+++ 01-color-switcher.b94ca142.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body,n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));a.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0,a.style.backgroundColor=""}))}();
+//# sourceMappingURL=01-color-switcher.b94ca142.js.map

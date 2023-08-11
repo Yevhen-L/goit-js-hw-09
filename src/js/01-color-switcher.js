d5b412d2e73721ba5bff3c42e149af04e36598ad
Yevhen-L <ujeen2101@gmail.com>
2023-08-11 15:06:22 +0300

    add code task 1 to 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  body.style.backgroundColor = '';
+});
 
